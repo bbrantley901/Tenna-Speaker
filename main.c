c6ec3fba88c9c9a19e4292ad18445cd99f18e38c
Bryce Brantley <bryce.c.brantley@gmail.com>
2025-10-22 23:51:25 -0500

    Some refactoring. Added build script

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
+/*
+TODO: Add button support with callback functions 
+      
+*/
+
 /* INCLUDES */
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "tenna_talking.h"
 #include "pico/audio_i2s.h"
 
 /* CONSTANTS */
+
+/* Audio Data Constants */
+#include "tenna_talking.h"
+
 #define I2S_DATA_PIN    (19U)
 #define I2S_BCLK_PIN    (20U)
 #define I2S_LRCLK_PIN   (21U)
@@ -18,24 +26,40 @@ static const int16_t *audio_samples = (const int16_t *)tenna_talking;
 /* Sample Size is array length / 2 ; 2 bytes/sample*/
 static const size_t audio_sample_count = tenna_talking_len / 2;
 
-/* PRIVATE VARIABLES */
-static uint32_t volume = 32; // no gain to start
-
-audio_i2s_config_t config = {
-    .data_pin = I2S_DATA_PIN,
-    .clock_pin_base = I2S_BCLK_PIN,
-    .dma_channel = 0,
-    .pio_sm = 0
+struct audio_file {
+    const int16_t *samples;
+    size_t sample_count;
 };
+
+
+/* PRIVATE VARIABLES */
+static struct audio_file audio_file_list[] = {
+    {
+        .samples = (const int16_t *)tenna_talking,
+        .sample_count = (sizeof(tenna_talking) / 2)
+    },
+};
+static struct audio_file *current_audio_file; 
+static audio_buffer_pool_t *ap = NULL;
+static uint32_t volume = 32;
 
 /* PRIVATE FUNCTION PROTOTYPES */
 static audio_buffer_pool_t *init_audio(void);
+static void play_audio(const int16_t *audio_samples, size_t sample_count);
 
 /* PUBLIC FUNCTION IMPLEMENTATIONS */
 int main()
 {
     stdio_init_all();
-    audio_buffer_pool_t *ap = init_audio();
+    ap = init_audio();
+    current_audio_file = &audio_file_list[0];
+    while(1)
+    {
+        play_audio(current_audio_file->samples, current_audio_file->sample_count);
+        sleep_ms(2000);
+    }
+
+#if 0
     size_t pos = 0;
     while (true)
     {
@@ -51,7 +75,32 @@ int main()
         buffer->sample_count = count;
         give_audio_buffer(ap, buffer);
     }
+#endif 
     return 0;
+}
+
+static void play_audio(const int16_t *audio_samples, size_t sample_count)
+{
+    size_t pos = 0;
+    audio_buffer_t *buffer = NULL;
+    int16_t *samples = NULL;
+    size_t count = 0;
+    while (pos < sample_count) { // While there are samples left to play
+        buffer = take_audio_buffer(ap, true); 
+        samples = (int16_t *) buffer->buffer->bytes;
+        count = buffer->max_sample_count;
+        for (int i = 0; i < count; i++) { // Fill audio buffer with 256 bytes of samples
+            int16_t s = audio_samples[pos++];
+            samples[i] = s;
+            samples[i+1] = s; 
+            if (pos >= audio_sample_count) {
+              buffer->sample_count = i; //Only process the samples we have
+              break; // No more samples left; exit allocation of audio buffer
+            }
+        }
+        buffer->sample_count = count;
+        give_audio_buffer(ap, buffer); // Play audio buffer
+    }
 }
 
 static audio_buffer_pool_t *init_audio(void)
@@ -76,6 +125,7 @@ static audio_buffer_pool_t *init_audio(void)
         .dma_channel = 0,
         .pio_sm = 0
     };
+
     output_format = audio_i2s_setup(&audio_format, &config);
     if (!output_format) {
         panic("I2S setup failed");
---
+++ tools/build_project.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ERROR_LEVEL=0
+export PICO_EXTRAS_PATH="$HOME/libraries/pico-extras"
+export PICO_SDK_PATH="$HOME/.pico-sdk/pico-sdk/2.1.0"
+
+echo "PICO_EXTRAS_PATH is set to: $PICO_EXTRAS_PATH"
+echo "PICO_SDK_PATH is set to: $PICO_SDK_PATH"
+function cmake_configure() {
+  echo "Configuring CMake project"
+  mkdir -p build
+  cd build
+  cmake ../
+  ERROR_LEVEL=$?
+  if [ $ERROR_LEVEL -ne 0 ]; then
+    echo "Failed to configure CMake, exit status: $ERROR_LEVEL"
+    exit $ERROR_LEVEL
+  fi
+}
+
+function build_project() {
+  echo "Building project"
+  cmake --build .
+  ERROR_LEVEL=$?
+  if [$ERROR_LEVEL -ne 0]; then
+    echo "Failed to build project, exit status: $ERROR_LEVEL"
+    exit
+  fi
+}
+
+cd ..
+
+if [ -d "build" ]; then
+  echo "build directory exists"
+else
+  echo "Creating build directory"
+fi
+
+cmake_configure
+build_project
+echo "Build completed successfully"
+
+
